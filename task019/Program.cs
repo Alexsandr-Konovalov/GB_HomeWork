719fc0b5d59c4c4f1b1a74acb2f700aa8ca4f07f
Alexandr Konovalov <expert.86@inbox.ru>
2022-12-21 10:54:08 +0300

    add tasks 19, 21, 23

---
+++ task019/Program.cs
@@ -0,0 +1,40 @@
+﻿//Задача №19
+//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+Console.Write("Введите пятизначное число -> ");
+int num = int.Parse(Console.ReadLine()!);
+string strNum = $"{num}";
+
+if (strNum[0] == strNum[4] && strNum[1] == strNum[3])
+{
+    Console.Write("да");
+}
+else
+{
+    Console.Write("нет");
+}
+
+// другой способ
+
+Console.Write("Введите пятизначное число -> ");
+int num = int.Parse(Console.ReadLine()!);
+int num1 = num;
+int revers = 0;
+int i = 0;
+while (num > 0)
+{
+    i = num % 10;
+    revers = revers * 10 + i;
+    num = num / 10;
+}
+if (num1 == revers)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.Write("нет");
+}
---
+++ task021/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача №21
+// Напишите программу, которая принимает на вход координаты двух точек
+// и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Введите координаты первой точки: ");
+Console.Write("По оси Х -> ");
+double XA = int.Parse(Console.ReadLine()!);
+Console.Write("По оси Y -> ");
+double YA = int.Parse(Console.ReadLine()!);
+Console.Write("По оси Z -> ");
+double ZA = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите координаты второй точки: ");
+Console.Write("По оси Х -> ");
+double XB = int.Parse(Console.ReadLine()!);
+Console.Write("По оси Y -> ");
+double YB = int.Parse(Console.ReadLine()!);
+Console.Write("По оси Z -> ");
+double ZB = int.Parse(Console.ReadLine()!);
+
+double AB = Math.Sqrt(Math.Pow(XB - XA, 2) + Math.Pow(YB - YA, 2) + Math.Pow(ZB - ZA, 2));
+Console.Write(AB);
