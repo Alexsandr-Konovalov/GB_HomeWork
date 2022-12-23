24b7f53614cebb798e4f06f7aaa190ab6eb27d6e
Alexandr Konovalov <expert.86@inbox.ru>
2022-12-23 23:03:29 +0300

    added task038

---
+++ task038/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 38: Задайте массив вещественных чисел.
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+double[] numbers = { 1.4, 2.2, 3.8, 4.1, 5.9 };
+double numMax = numbers[0];
+double numMin = numbers[0];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] >= numMax) numMax = numbers[i]; i++;
+}
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] <= numMin) numMin = numbers[i]; i++;
+}
+
+Console.WriteLine($"разницa между максимальным и минимальным элементов массива -> {numMax - numMin}");
