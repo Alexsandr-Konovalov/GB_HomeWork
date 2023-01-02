5bcb1f6caccec33a6ec8298d5562a8e48b81f8e2
Alexandr Konovalov <expert.86@inbox.ru>
2023-01-02 20:37:57 +0300

    taskExpert002

---
+++ taskExpert002/Program.cs
@@ -0,0 +1,29 @@
+﻿// Доп. задача Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// Пример:
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+Console.Write("Введите любое целое число -> ");
+int numberInt = Convert.ToInt32(Console.ReadLine()!);
+int d = 0;
+string j = "";
+
+
+while (numberInt >= 1)
+{
+    d = numberInt % 2;
+    numberInt = numberInt / 2;
+    j =  j + Convert.ToString(d);
+}
+Console.WriteLine(j);
+
+string[] array = new string[j.Length];
+
+for (int i = 0; i < j.Length - 1; i++)
+{
+    array[0] = array[j.Length - 1 - i];
+    
+}
+Console.Write(array);
+
+
